a9bc4c317648f448b30b82c1464463a983739306
shalvad10 <shalvadoliashvili@gmail.com>
2022-06-18 10:03:16 +0400

    change connection URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'http://20.73.255.80:8088',
-  connectionURL: 'ws://20.73.255.80:10010'
+  apiURL: 'http://113.30.191.222:8088',
+  connectionURL: 'ws://113.30.191.222:10010'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiURL: 'http://20.73.255.80:8088',
-  connectionURL: 'ws://20.73.255.80:10010'
+  apiURL: 'http://113.30.191.222:8088',
+  connectionURL: 'ws://113.30.191.222:10010'
 };
 
 /*
